e9f1599c42397258a7df8016b0d222e74edb2898
Ben Sehnert <bps53@protonmail.com>
2021-04-24 20:01:14 -0400

    Added skills and about me sections. These will reqiuire some planning and lots of css, animations, etc.
    Still thinking we can do this page without
    using baas like firebase fireship. most of the data is assuredly static.

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -8,7 +8,33 @@ export default class Contact extends Component {
   render() { 
     return (
       <div className="app-contact">
-        How to contact dah buhl
+        <h1>Contact me:</h1>
+
+
+        <p>I can be reached by the following means:</p>
+        <ol>
+          <li><strong>Email:</strong>bps53@protonmail.com</li>
+          <li><strong>Call:</strong>215-837-2319</li>
+          <li><strong>Private Message:</strong>Linkdin, github</li>
+        </ol>
+        <p>I'm happy to discuss the following: </p>
+        <ol>
+          <li>Paid and pro-bono work opprotunities</li>
+          <li>Constructive critisim</li>
+          <li>Friendly and/or instructive career advice</li>
+        </ol>
+
+
+        <p><strong>Note: </strong> I am currently using Robo-call to screen scam calls, as I am frequently harrased with these on my mobile phone. <br/>
+        For this reason, If you try my cell and are unable to get through, please select an alternate means from the above list <br/>
+        I am attentitive to my cell-phone at most times of the day, and personally have not been made aware of recruiters or hiring manager being unable to get through <br/>
+        However, I would not rule it out as a possibility.</p>
+        <br/>
+        <p><strong>Best times to contact me:</strong> Normal business hours Monday-Friday</p>
+
+
+        <h3>On Vacation? not currently.</h3>
+        <p>Feel free to reach out at your earliest desired convenience.</p>
       </div>
     );
   }
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -5,10 +5,61 @@ export default class Skills extends Component {
     super(props);
     this.state = {  }
   }
-  render() { 
+  render() {
+
     return (
       <div class="app-skills">
-        Use grid to list tiles with heading and sub text about what you can do with each skill
+        What a terrible use of react!
+        <div class="view-box">
+          <div>
+            <h3>Technical Skills</h3>
+            Think you should have sub headings: scripting, web frameworks, general programming...
+            <ul>
+              <li>Python 2 and 3</li>
+              <li>JS family: typescript, ECMA6, JSX</li>
+              <li>HTML5 + CSS3, Jquery, Ajax</li>
+              <li>AngularJS, ReactJS, VueJS</li>
+              <li>Django, Spring MVC, Ruby on Rails, ASP.net, PHP</li>
+              <li>Bash, Batch, Awk, C family (C,C++ & C#)</li>
+              <li>SQL: MSQL, POSTgreSQL (PSQL)</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Software practice/ Theory:</h3>
+            <ul>
+              <li>Information Systems management and design</li>
+              <li>UML- unified modeling language</li>
+              <li>Agile, scrum, kanban, traditional sdlc (waterfall) </li>
+              <li>Project management</li>
+              <li>Requirements engineering</li>
+              <li>Reliability and testing (clean room methodology, safety critical systems)</li>
+              <li>Computer Science: Theory of computing, Data structures and algorithms, Programming langauge design</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Tooling:</h3>
+            <ul>
+              <li>Visual Studios Code</li>
+              <li>Eclipse</li>
+              <li>VIM</li>
+              <li>Postman</li>
+              <li>Docker, mostly for connecting database containers to my rest APIs</li>
+              <li>Database management systems:  Microsoft SQL server management studio (SSMS), Oracle SQL Developer</li>
+            </ul>
+            
+          </div>
+        </div>
+
+        Use grid to list tiles with heading (name of the skill) and text (elaboration), click on a tile to maximize/ show more info.
+        <br/>
+        for now
+        <br/>
+        
+        Software practices: Information System Design and management, UML (unified Modeling Language), Requirements engineering, documentation, Reliability Engineering, Project management
+
+
+
+
       </div>
       );
   }
