32ef7b9ccdd7a30c221dc5c0371d994b64e1cccd
Ben Sehnert <bps53@protonmail.com>
2021-04-27 13:42:16 -0400

    Doesnt look so hot, but we have 2x level nested json data displaying in the skills section. Needs a bit of css and designing to get it working happily. Plan on making it a dropdown where you see more info the more you scroll over

--- src/ContentBox.jsx
+++ src/ContentBox.jsx
@@ -5,18 +5,57 @@ export default class ContentBox extends Component {
     super(props);
     this.state = {  }
   }
+
+  renderList(objList) {
+    /**
+     * @param objList <{title : string, content : string []} []>
+     * list of objects being looped over. Its assumed they follow
+     * 
+     * render a nested list of descriptor objects
+     * useful for managing complex, categorical data- skills -> programming -> web -> javascript -> angular 
+     */
+    // console.log(objList);
+    let complexObjects = [];
+    let display = []   
+    for (let item of objList) {
+      if(typeof(item) != "string") {
+        complexObjects.push(item);
+      } else {
+        display.push(item)
+      }
+
+    }
+
+    if(complexObjects) {
+      for (let subheading of complexObjects) {
+        display.push(
+          <li>
+            <h4>{subheading.title}</h4>
+            <ul>
+              {subheading.content.map(item => <li>{item}</li>)}
+            </ul>
+          </li>
+
+        )
+      } 
+    }
+
+    display = display.map(subject => <li>{subject}</li>)
+
+     return (display)
+  }
+
   render() {
-    const title = this.props.title ? this.props.title : "generic title";
-    const body = this.props.body ? this.props.body : ["g","e", "n", "e", "r", "i", "c", "b", "o", "d", "y"]; 
+    const title = this.props.content.title;
+    const body = this.props.content.content;
+
     return (
       <div class={this.props.position}>
         <h1>{title}</h1>
 
-
         <ul>
-          { body.map(sub => <li>{sub}</li>) }
+          {this.renderList(body)}       
         </ul>
-        
       </div>
     );
   }
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,97 +1,44 @@
 import React, { Component } from 'react';
 import ContentBox from './ContentBox';
-
+import data from './portfolio.json';
 
 export default class Skills extends Component {
   constructor(props) {
     super(props);
     this.state = {  }
   }
+
   render() {
-    const programmingSkills = ["Python","Typescript/Javascript","HtML5/CSS3","frameworks: AngularJS, ReactJS, Django, Spring, Ruby on Rails","general programming: C/C#/C++, Java, PHP","Scripting: Bash, Batch, AWK, perl", "Database: SQL,noSQL"];
-    const softwareSkills = ["Information Systems Design and management", "UML", "that other thing", "Requirements management", "Documentatiion", "Testing"];
-    const generalSkills = [3,4,5,6,7];
-    const developmentTools = ["dev environments: VSCode, Thonny, Eclipse, Vim","Testing: ","Postman","Docker", "Microsoft SQL Server Express", "Oracle RDBMS"];
+    
+
+    
+    const programmingSkills = data["skills"][0];
+    const softwareSkills = data["skills"][1]; 
+    const developmentTools = data["skills"][2]; 
 
 
     return (
-      <div class="app-skills">
+      <div className="app-skills">
         
         <section>
-          <div className="main-container">
+          <div class="main-container">
             <div class="heading-wrapper">
               <h3>Skills:</h3>
             </div>
           </div>
 
-          <div className="intro-wrapper">
-            {/* <div className="nav-wrapper">Nav</div> */}
+          <div class="intro-wrapper">
             
-            <ContentBox position="left-column" title="Progamming skills" body={programmingSkills}/>
+            <ContentBox position="left-column" content={programmingSkills}/>
 
-            <ContentBox position="middle-column" title="Software skills" body={softwareSkills}/>
+            <ContentBox position="middle-column" content={softwareSkills}/>
 
-            <ContentBox position="right-column" title="developer skills" body={developmentTools}/>
+            <ContentBox position="right-column" content={developmentTools}/>
 
           </div>
         </section>
         
-        {/* this is the flex box */}
-        
-        <div></div>
-        <div></div>
-        <div></div>
-
-        {/* What a terrible use of react!
-        <div class="view-box">
-          <div>
-            <h3>Technical Skills</h3>
-            Think you should have sub headings: scripting, web frameworks, general programming...
-            <ul>
-              <li>Python 2 and 3</li>
-              <li>JS family: typescript, ECMA6, JSX</li>
-              <li>HTML5 + CSS3, Jquery, Ajax</li>
-              <li>AngularJS, ReactJS, VueJS</li>
-              <li>Django, Spring MVC, Ruby on Rails, ASP.net, PHP</li>
-              <li>Bash, Batch, Awk, C family (C,C++ & C#)</li>
-              <li>SQL: MSQL, POSTgreSQL (PSQL)</li>
-            </ul>
-          </div>
-          <div>
-            <h3>Software practice/ Theory:</h3>
-            <ul>
-              <li>Information Systems management and design</li>
-              <li>UML- unified modeling language</li>
-              <li>Agile, scrum, kanban, traditional sdlc (waterfall) </li>
-              <li>Project management</li>
-              <li>Requirements engineering</li>
-              <li>Reliability and testing (clean room methodology, safety critical systems)</li>
-              <li>Computer Science: Theory of computing, Data structures and algorithms, Programming langauge design</li>
-            </ul>
-          </div>
-          <div>
-            <h3>Tooling:</h3>
-            <ul>
-              <li>Visual Studios Code</li>
-              <li>Eclipse</li>
-              <li>VIM</li>
-              <li>Postman</li>
-              <li>Docker, mostly for connecting database containers to my rest APIs</li>
-              <li>Database management systems:  Microsoft SQL server management studio (SSMS), Oracle SQL Developer</li>
-            </ul>
-            
-          </div>
-        </div>
-
-        Use grid to list tiles with heading (name of the skill) and text (elaboration), click on a tile to maximize/ show more info.
-        <br/>
-        for now
-        <br/>
-        
-        Software practices: Information System Design and management, UML (unified Modeling Language), Requirements engineering, documentation, Reliability Engineering, Project management
-
-
- */}
+  
 
       </div>
       );
