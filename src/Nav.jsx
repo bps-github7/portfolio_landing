f3d329197c349a0d570560350950db879dd68543
Ben Sehnert <bps53@protonmail.com>
2021-04-29 17:44:37 -0400

    Worked on animated dropdown menu, All it needs in debugging and css to make the animations more fluid. the info is all there (except links to the portfolio projects, because these arent deployed yet, we dont have any links to put there)
    added some filler content to the about section, this needs consideration in
    terms of styling/ responsiveness
    (see note in comments) and would
    like to get this data from the json
    instead in the future.

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -43,7 +43,10 @@ const TitleItem = props => {
   );
 }
 
+
 function DropdownMenu() {
+
+  //suggest export to a class based component so you dont have to make all these menus into local functional conponents. sort of messy. sort of no big deal yeah?
 
   const [activeMenu, setActiveMenu] = useState('main');
 
@@ -58,53 +61,115 @@ function DropdownMenu() {
   }
 
   return (
-      <div className="dropdown">
-          <CSSTransition 
-              in={activeMenu === 'main'} 
-              unmountOnExit 
-              timeout={500}
-              classNames="menu-primary">
-                  <div className="menu" >
-                      
-                      {/* TODO: make this a sub-dropdown, it appears below vertically and lists projects like
-                      -IVDB
-                      -ProgrammingBlog
-                      -Oral History
-                      -Cookin */}
-                      <DropdownItem
-                      leftIcon={ <CaretIcon/> }
-                      
-                      >Front end</DropdownItem>
-                      <DropdownItem
-                          leftIcon={ <CogIcon/> }
-                          rightIcon={ <ChevronIcon/> }
-                          goToMenu="backend">
-                          Back end
-                      </DropdownItem>
-                  </div>
-          </CSSTransition>
-      
-          <CSSTransition 
-              in={activeMenu === 'backend'} 
-              unmountOnExit 
-              timeout={500}
-              classNames="menu-secondary">
-                  <div className="menu" >
-                      <DropdownItem
-                          leftIcon={ <ChevronIcon/> }
-                          goToMenu="main"
-                      />
-                      <DropdownItem
-                          leftIcon={ <CogIcon/> }
-                          rightIcon={ <ChevronIcon/> }
-                          >
-                          Here some Rest APIs i made
-                      </DropdownItem>
-                  </div>
-          </CSSTransition>
+    <div className="dropdown">
 
-      </div>
-  );
+
+      <CSSTransition 
+        in={activeMenu === 'main'} 
+        unmountOnExit 
+        timeout={500}
+        classNames="menu-primary">
+        <div className="menu" >
+          <DropdownItem
+            leftIcon={ <CaretIcon/> }
+            goToMenu="frontend"> 
+            Front-end Projects
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={ <CogIcon/> }
+            rightIcon={ <ChevronIcon/> }
+            goToMenu="backend">
+            Back-end Projects
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={ <PlusIcon/> }
+            rightIcon={ <ChevronIcon/> }
+            goToMenu="testing">
+            Testing
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition 
+        in={activeMenu === 'frontend'} 
+        unmountOnExit 
+        timeout={500}
+        classNames="menu-second">
+        <div className="menu" >
+          <DropdownItem
+            leftIcon={ <ChevronIcon/> }
+            goToMenu="main">
+            Go back
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={ <CogIcon/> }>
+            Internet Videogame Database
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={ <MessengerIcon/> }
+            rightIcon={ <ChevronIcon/> }>
+            Oral History Blog 
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition 
+        in={activeMenu === 'backend'} 
+        unmountOnExit 
+        timeout={500}
+        classNames="menu-third">
+        <div className="menu" >
+          <DropdownItem
+            leftIcon={ <ChevronIcon/> }
+            goToMenu="main">
+            Go back
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={ <CogIcon/> }
+            rightIcon={ <ChevronIcon/> }>
+            .Net + MSSQL REST api 
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={ <BoltIcon/> }>
+            Java Spring + PSQL REST api 
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+ 
+      <CSSTransition 
+        in={activeMenu === 'testing'} 
+        unmountOnExit 
+        timeout={500}
+        classNames="menu-fourth">
+        <div className="menu" >
+          <DropdownItem
+            leftIcon={ <ChevronIcon/> }
+            goToMenu="main"/>
+            <DropdownItem
+              leftIcon={ <CogIcon/> }
+              rightIcon={ <ChevronIcon/> }>
+              Complex number calculator 
+            </DropdownItem>
+            <DropdownItem
+              leftIcon={ <BoltIcon/> }>
+              Binary Conversion Calculator
+            </DropdownItem>
+            <DropdownItem
+              leftIcon={ <BoltIcon/> }>
+              Chess Move Generator 
+            </DropdownItem>
+            <DropdownItem
+              leftIcon={ <BoltIcon/> }>
+              MS-test example unit tests 
+            </DropdownItem>
+            <DropdownItem
+              leftIcon={ <BoltIcon/> }>
+              N-unit example unit tests 
+            </DropdownItem>
+          </div>
+        </CSSTransition> 
+     </div>
+    );
 }
 
 
@@ -119,6 +184,7 @@ function NavBar(props) {
 }
 
 const NavItem = (props) => {
+  /* Why did u need to make 3 seperate components? maybe unnesseceary! */
   return (
       <li className="nav-item">
           <a href={props.url} className="icon-button">
