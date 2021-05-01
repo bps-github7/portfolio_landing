d22b971b33f7c52abd96cbd75a5feeeda032129f
Ben Sehnert <bps53@protonmail.com>
2021-05-01 14:16:18 -0400

    Not really getting a lot done today The transition to pure vim has been slow and painful.
    hopefully done soon,
    working on the JSON file as usual.
    
    Next time using JSON use a more uniform structure!
    and object naming conventions- strings
    should be camelCase.
    
    downloaded a repo from youtube
    Ben Awad, I was going to refactor his
    TODO component for VIM lesson
    but never got around to it. These components could come in handy

--- src/About.jsx
+++ src/About.jsx
@@ -1,29 +1,94 @@
 import React, { Component } from 'react';
+import ThreePaneInfoPanel from './ThreePaneInfoPanel';
+import data from './portfolio.json';
+
 
 export default class About extends Component {
+  
+  show=true;
+  topic="";
+  currentSelected;
+
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      defaultOrganization: true,
+    }
   }
+
+  switchOrganization = () => {
+    this.setState({
+      defaultOrganization : !this.state.defaultOrganization
+    })
+  }
+
+  showInfoPanel(toShow) {
+      if (toShow === "music") {
+        this.topic = "music";
+        this.currentSelected = data["about"]["music"]
+        this.setState({
+          defaultOrganization : false
+        })
+      }
+      else if (toShow === "excersise") {
+        this.topic = "excersis";
+        this.currentSelected = data["about"]["general"]
+        this.setState({
+          defaultOrganization : false 
+        })
+      }
+      else if (toShow === "cooking") {
+        this.topic = "cooking";
+        this.currentSelected = data["about"]["cooking"];
+        this.setState({
+          defaultOrganization : false
+        })
+ 
+      }
+    }
+
+
   render() { 
     return (
       <div class="app-about">
-   <section>
+        <section>
           <div class="main-container">
             <div class="heading-wrapper">
-              <h3>About:</h3>
+              <h3>About</h3>
             </div>
           </div>
 
-          <div class="intro-wrapper" style={{justifyContent:"center"}}>
-            
+          { this.state.defaultOrganization ?
+          <div class="intro-wrapper-rows">
 
+            <div class="top-row">
+              <span>Music</span>
+              <button 
+              onClick={() => this.showInfoPanel("music")}>
+                Show
+              </button>
+            </div>
 
+            <div class="middle-row">
+                <span>Excersis</span>
+                <button
+                  onClick={() => this.showInfoPanel("excersise")}>
+                  Show
+                </button>
+            </div>
+
+            <div class="bottom-row">
+                <span>Cooking</span>           
+                <button
+                onClick={()=>this.showInfoPanel("cooking")}>
+                  Show
+                </button>
+            </div>
           </div>
+        // : <div>Bisk</div>}
+        : <ThreePaneInfoPanel info={this.currentSelected} orientation={this.switchOrganization}/>} 
         </section>
-
-   
-               
+        
       </div>
       );
   }
