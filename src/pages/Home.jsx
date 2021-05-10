6e40db9a35e5ae8a06af8b0088e8ba1569090c13
Ben Sehnert <bps53@protonmail.com>
2021-05-10 16:05:32 -0400

    Broke down index.scss into three smaller files for functional areas of the site. Going to be working thru a RWD course on W3schools to get this site responsive so we can get it live ASAP [and get a joob in turn ASAP]

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,6 @@ export default class Home extends Component {
         </h1>
         <h3>Aspring Software Developer Located in West Philadelphia/University City</h3>
         {/* <p>Click the button below to explore the site</p> */}
-     
         <section class="s1">
           <div className="main-container">
             Some stuff
