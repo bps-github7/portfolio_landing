2a6bfc579c25f331311ca6d69d42e4c7f8d737c3
Ben Sehnert <bps53@protonmail.com>
2021-05-19 09:04:57 -0400

    Removed Skills and About components, moving towards the same with experience and contacts, by passing the components used within these as children to their respective Route components (note this allows dynamic props unlike the 'component' prop on the single element Route selector. greatly simplifies everything while we move towards styling and adding animtion and responsiveness (repeating myself, repeating myself)*10.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './components/Nav';
-import About from './components/About';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Home from './components/Home';
-import Skills from './components/Skills';
 import Contact from './components/Contact';
-
+import InfoPanel from './components/InfoPanel';
+import data from './data.json';
 
 
 function App() {
@@ -18,9 +17,15 @@ function App() {
         <Nav/>
         <Switch>
           <Route exact path='/home' component={Home}/>
-          <Route exact path='/contact' component={Contact}/>
-          <Route exact path='/skills' component={Skills}/>
-          <Route exact path='/about' component={About}/>
+          <Route exact path='/contact'>
+            <Contact contact={data.contact}/>
+          </Route>
+          <Route exact path='/skills'>
+            <InfoPanel info={data.skills}/>
+          </Route>
+          <Route exact path='/about'>
+            <InfoPanel info={data.about}/>  
+          </Route>
           <Route exact path='/projects' component={Projects}/>
           <Route exact path='/experience' component={Experience}/>
         </Switch>
