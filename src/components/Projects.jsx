de43b3ff5945fdade86cd8d37b689df83f31a8a0
Ben Sehnert <bps53@protonmail.com>
2021-05-27 09:07:04 -0400

    Cleared the last implementation from projects. Do a bit of research on Semantic UI before jumping in head first, we want to better organize our css, so any added library ought to assist with that objective.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,45 +8,13 @@ export default class Projects extends Component {
   }
   render() { 
     /*
-    TODO: use github api to fetch this data and display it. to do otherwise (like below) is ChIlDsPlAy and very non DRY
+    TODO: use github api to fetch this data and display it. to do otherwise (like below) s
     */
 
     return (
       <section className="projects">
-      {
-        // this loop gives us the types of categories of projects ```data.projects.{key}```
-        Object.keys(data.projects)
-        .map(
-          (category, i) => (
-            <section key={i}>
-              <h2>{category}</h2>
-              {/*  Make a button that shows/ hides the projects for a certain category*/}
-               
-              {
-                // this loop accesses the projects in a category ```data.projects.front-end.{key}```
-                Object.keys(data.projects[category])
-                .map((title, i) => 
-                (
-                  <section>
-                    {  
-                      data.projects[category][title].link ? 
-                      <a href={data.projects[category][title].link}>
-                        <h4>{title}</h4>
-                      </a>:
-                      <h4>{title}</h4>
-                    }
-                    <p>
-                      {data.projects[category][title].description}
-                    </p>
-                    <a href={data.projects[category][title].src}>view source code</a>
-                  </section>
-                ))}
-
-            </section>
-          )
-        )
-       }
-      </section>
+        Projects go here
+     </section>
     );
   }
 }
