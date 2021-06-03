60ff5695c691d167629922a37fe4166c85068e63
Ben Sehnert <bps53@protonmail.com>
2021-06-03 10:34:10 -0400

    First steps in refactoring the Panel component was to get the two components (ImagePicker {aptly renamed} and ImageSlider) in their own Top level component, called ImageViewer. I am hoping this will map one to one with the props that the previous ImageViewer expected.

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -10,7 +10,6 @@ const Panel = (props) => {
     "https://i.pinimg.com/236x/b6/21/07/b62107d70659823c6f409480f7435d14--watercolor-food-watercolor-print.jpg",
     "https://i.pinimg.com/236x/8a/61/c1/8a61c1ca0d1a20db922aa5e3a8493d0e--watercolour-flowwatercolor-print.jpg",
     "https://i.pinimg.com/236x/86/d6/46/86d646ce2f92efd7890843ef5af9e700.jpg",
-    "https://i.pinimg.om/236x/f6/4d/d7/f64dd7eb8df298195d61569b9634f982--watercolor-sketch-watercolor-kiwi.jpg"
   ];
 
   /* The P.O.C works well, so its time to make a layout...
@@ -30,27 +29,30 @@ const Panel = (props) => {
   return (
     <section className="panel">
       <h1>{props.title}</h1>
-      <Picker images={imgs} setSelectedImage={setSelectedImage}/>
+      <ImageViewer imgs={imgs} selectedImage={selectedImage} setSelectedImage={setSelectedImage}/>
+      { selectedImage && <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage}/>}
+    </section>
+  );
+}
+
+const ImageViewer = ({imgs, selectedImage, setSelectedImage}) => {
+  return (
+    <section className="imageViewer">
+      <ImagePicker images={imgs} setSelectedImage={setSelectedImage}/>
       <ImageSlider images={imgs} setSelectedImage={setSelectedImage}/>
       { selectedImage && <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage}/>}
     </section>
   );
-  
-
 }
 
-const Picker = ({images, setSelectedImage}) => {
-
-  /* This is merely a proof of concept, so we dont care that imgs are hardcoded pictures of fruits and whatnot */
-
+const ImagePicker = ({images, setSelectedImage}) => {
   return(
     <section class="default">
-      {
-        images.map((item) => (
+      { images.map((item) => (
           <section onClick={() => setSelectedImage(item)}>
             <img src={item} alt="" />
-          </section>
-        ))
+          </section>)
+        )
       }
     </section>
   )
