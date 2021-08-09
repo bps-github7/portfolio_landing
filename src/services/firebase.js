6aa31035426374c7295b75eb82887807bf715bc0
Ben Sehnert <bps53@protonmail.com>
2021-08-09 12:34:30 -0400

    fixing remaining pck management issues. according to yarn upgrade-interactive, all my deps are up to date. So really this should build.

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,7 +8,7 @@ var firebaseConfig = {
 	// storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 	// messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 	// appId: process.env.REACT_APP_FIREBASE_MESSAGING_APP_ID
-	apiKey: "process.env.REACT_APP_FIREBASE_KEY", 
+	apiKey: "@process.env.REACT_APP_FIREBASE_KEY", 
 	authDomain: "process.env.REACT_APP_FIREBASE_DOMAIN",
 	projectId: "process.env.REACT_APP_FIREBASE_PROJECT_ID",
 	storageBucket: "process.env.REACT_APP_FIREBASE_STORAGE_BUCKET",
